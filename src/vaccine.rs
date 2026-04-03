08516ba4876fde334fd1211c6782f4f481199c4c
Scott Olesen <ulp7@cdc.gov>
2026-04-03 14:51:45 -0400

    comments

--- src/vaccine.rs
+++ src/vaccine.rs
@@ -1,15 +1,20 @@
 use ixa::prelude::*;
 
+/// The act of administering a vaccine
 pub struct VaccineAdministration<U, P> {
+    /// The "time" of vaccination could be a literal clock time, or it could be something more abstract
     pub time: U,
+    /// Could be general like "flu", or it could be a specific manufacturer, dosage, lot, etc.
     pub product: P,
 }
 
+/// Very general interface for vaccines
 pub trait Vaccine<T: Entity, U, P, E>: PluginContext {
     fn vaccinate(&mut self, entity_id: EntityId<T>, time: U, product: P) -> Result<(), E>;
 
+    /// Vaccination history is a collection of vaccination events
     fn get_vaccination_history(
         &mut self,
         entity_id: EntityId<T>,
-    ) -> Result<&Vec<VaccineAdministration<U, P>>, E>;
+    ) -> Result<&impl IntoIterator<Item = VaccineAdministration<U, P>>, E>;
 }
--- src/vaccine_basic.rs
+++ src/vaccine_basic.rs
@@ -3,19 +3,19 @@ use crate::vaccine::{Vaccine, VaccineAdministration};
 use ixa::prelude::*;
 use std::collections::HashMap;
 
+// Concrete data storage: from PersonId to a vector of vaccine administrations
 define_data_plugin!(VaccineAdministrationHistoryPlugin, HashMap<PersonId, Vec<VaccineAdministration<(), ()>>>, HashMap::new());
 
+// Concrete implementation of Vaccine, where I don't care for the vaccination time, product, or error
 impl Vaccine<Person, (), (), ()> for Context {
     fn vaccinate(&mut self, entity_id: PersonId, time: (), product: ()) -> Result<(), ()> {
         let admin = VaccineAdministration { time, product };
-        let data = self.get_data_mut(VaccineAdministrationHistoryPlugin);
 
-        // initialize if needed
-        if data.contains_key(&entity_id) {
-            data.get_mut(&entity_id).ok_or(())?.push(admin);
-        } else {
-            data.insert(entity_id, vec![admin]);
-        }
+        // push onto an existing vector, or insert and return a new one
+        self.get_data_mut(VaccineAdministrationHistoryPlugin)
+            .entry(entity_id)
+            .or_default()
+            .push(admin);
 
         Ok(())
     }
@@ -23,15 +23,17 @@ impl Vaccine<Person, (), (), ()> for Context {
     fn get_vaccination_history(
         &mut self,
         entity_id: PersonId,
-    ) -> Result<&Vec<VaccineAdministration<(), ()>>, ()> {
+    ) -> Result<&impl IntoIterator<Item = VaccineAdministration<(), ()>>, ()> {
+        // if no existing history, initialize with empty vector
         Ok(self
             .get_data_mut(VaccineAdministrationHistoryPlugin)
             .entry(entity_id)
-            .or_insert(vec![]))
+            .or_default())
     }
 }
 
 pub trait VaccineBasic: Vaccine<Person, (), (), ()> {
+    /// Vaccinated if has had exactly one dose. Multiple doses are unexpected.
     fn is_vaccinated(&mut self, entity_id: PersonId) -> Result<bool, ()> {
         match self.get_vaccination_history(entity_id).iter().len() {
             0 => Ok(false),
